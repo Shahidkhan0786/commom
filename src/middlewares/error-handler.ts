f7486a1bcaaf1faf369a1f35308b347e91db4bb8
Shahid Khan <130751667+shahid-khan-codegenio@users.noreply.github.com>
2023-07-23 08:06:32 +0500

    add errors and middlewares

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response, NextFunction } from "express";
+import { ValidationError } from "express-validator";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { CustomError } from "../errors/custom-error";
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // console.log("something went wrong", err);
+  if (err instanceof CustomError) {
+    console.log("req validation error");
+    // console.log(err)
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+
+  res.status(400).send({
+    errors: [
+      {
+        message: err.message,
+      },
+    ],
+  });
+};

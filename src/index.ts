fdd71028ec37194d4777ebddf18c33b63bca2acd
Shahid Khan <130751667+shahid-khan-codegenio@users.noreply.github.com>
2023-07-23 07:39:51 +0500

    add some changes

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color{
+    red: number;
+    greeen: number;
+    blue: number
+}
+
+const color:Color = {
+    red: 10,
+    greeen: 20,
+    blue:10
+}
+
+console.log(color);
+
+export default color;   
